78d8119ed8af0477e81b874fff27423970d21370
Sergei Trofimovich <slyfox@inbox.ru>
2008-03-08 17:38:16 +0200

    Revert "PRIVATE COMMIT: added LAN server" All the stuff is implemented This reverts commit aa444d86eaf26af9f72a7742e015fe2372e0c35e.

--- src/server.c
+++ src/server.c
@@ -356,7 +356,6 @@ server_register_s2s_links (struct server * server)
 {
     assert (server);
     // TODO: FIXME: here can be server set to connect to
-    start_s2s_link (server, "192.168.0.3", 6969, "lol");
     return 0;
 }
 
