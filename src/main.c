be0e239b997fe5df5c6d493d66df45e84a9984db
Sergei Trofimovich <slyfox@inbox.ru>
2008-03-28 22:03:22 +0200

    fast fix: made server shutdown consistent
    Signed-off-by: Sergei Trofimovich <slyfox@inbox.ru>

--- src/main.c
+++ src/main.c
@@ -11,14 +11,14 @@ main (int argc, char** argv)
 {
     server = server_alloc ();
     if (!server
-        || server_configure (server, argc, argv)
-        || server_run (server)
-        || server_shutdown (server))
+        || server_configure (server, argc, argv))
     {
         fprintf (stderr, "%s: %s\n", argv[0], server_error (server));
         server_destroy (server);
         return EXIT_FAILURE;
     }
+    server_run (server);
+    server_shutdown (server);
     server_destroy (server);
     return EXIT_SUCCESS;
 }
